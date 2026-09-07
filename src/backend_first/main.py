1a9ad99fa5028df6fe47bf680106a950a721557f
Serhii <sersem0305@gmail.com>
2026-09-07 12:49:55 +0300

    feat: add job registration

--- src/backend_first/main.py
+++ src/backend_first/main.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
-from fastapi.responses import Response
+from fastapi import FastAPI, HTTPException, Header
+from pydantic import BaseModel, field_validator
+
+
+class Job(BaseModel):
+    title: str
+    company: str
+    location: str
+    salary: int
+    @field_validator('salary')
+    @classmethod
+    def check_salary(cls, value: int) -> int:
+        if value <= 0:
+            raise ValueError("salary must be greater than 0")
+        return value
 
 app = FastAPI()
-
-@app.get("/health")
-def health_check():
-    return Response(status_code=200)
+jobs_list = []
+@app.post("/jobs")
+def add_job(job: Job):
+    jobs_list.append(job.model_dump())
+    return jobs_list
